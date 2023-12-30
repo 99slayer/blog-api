8c46a717a94f68d04ab318a72428ea523ee44b0d
99slayer <21979862+99slayer@users.noreply.github.com>
2023-12-30 04:11:47 -0700

    Create document schemas

--- models/comment.js
+++ models/comment.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
-const CommentSchema = new Schema({});
+const CommentSchema = new Schema({
+	timestamp: { type: Date, required: true },
+	text: { type: String, required: true },
+});
+
+CommentSchema.virtual('timestamp_formatted').get(function () {
+	const dt = DateTime.fromJSDate(this.timestamp);
+	return dt.toLocaleString(DateTime.DATE_SHORT) + ' ' + dt.toLocaleString(DateTime.TIME_WITH_SECONDS);
+})
 
 module.exports = mongoose.model('Comment', CommentSchema);
--- models/post.js
+++ models/post.js
@@ -1,7 +1,20 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({});
+const PostSchema = new Schema({
+	title: { type: String, required: true },
+	posted_timestamp: { type: Date, required: true },
+	published_timestamp: { type: Date, default: null, required: true },
+	text: { type: String, required: true },
+	creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+	published: { type: Boolean, required: true },
+});
+
+PostSchema.virtual('published_timestamp_formatted').get(function () {
+	const dt = DateTime.fromJSDate(this.published_timestamp);
+	return dt.toLocaleString(DateTime.DATE_SHORT) + ' ' + dt.toLocaleString(DateTime.TIME_WITH_SECONDS);
+});
 
 module.exports = mongoose.model('Post', PostSchema);
