6e47cdf94b8ecaf9263624f6b46faa22bfb4a955
99slayer <21979862+99slayer@users.noreply.github.com>
2024-01-18 17:10:55 -0700

    Add new virtual property
    Added the posted_timestamp_formatted virtual property.
    
    Set the schemas toJSON option so virtuals could be sent as json.

--- models/post.js
+++ models/post.js
@@ -6,10 +6,15 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
 	title: { type: String, required: true },
 	posted_timestamp: { type: Date, required: true },
-	published_timestamp: { type: Date, default: null, required: true },
+	published_timestamp: { type: Date, default: null },
 	text: { type: String, required: true },
 	creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-	published: { type: Boolean, required: true },
+	published: { type: Boolean, default: false, required: true },
+}, { toJSON: { virtuals: true } });
+
+PostSchema.virtual('posted_timestamp_formatted').get(function () {
+	const dt = DateTime.fromJSDate(this.posted_timestamp);
+	return dt.toLocaleString(DateTime.DATE_SHORT) + ' ' + dt.toLocaleString(DateTime.TIME_WITH_SECONDS);
 });
 
 PostSchema.virtual('published_timestamp_formatted').get(function () {
