8ed086f5863394a156a0d2fcdcb065efd5cb084b
99slayer <21979862+99slayer@users.noreply.github.com>
2024-01-30 15:33:59 -0700

    Setup various middleware

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config();
 const cors = require('cors');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const apiRouter = require('./routes/api');
 const testRouter = require('./routes/test');
@@ -21,6 +22,16 @@ async function main() {
 	await mongoose.connect(mongoDB);
 }
 
+app.use(compression());
+app.use(helmet());
+
+const limit = require('express-rate-limit');
+const limiter = limit({
+	windowMs: 60000,
+	max: 40
+});
+app.use(limiter);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,7 +42,6 @@ app.use(cors({
 	]
 }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRouter);
 app.use('/test', testRouter);
@@ -45,7 +55,6 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
-	console.log(`ERROR MESSAGE: ${err.message}`);
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
 	res.status(err.status || 500);
